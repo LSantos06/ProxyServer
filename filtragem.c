c6519cb3b7a01f0b8f8a867214ed95080d3e8c0a
M-Tonin <marcosvtonin@hotmail.com>
2017-11-14 15:39:29 -0200

    procurando deny terms e url padrao

--- filtragem.c
+++ filtragem.c
@@ -5,40 +5,23 @@ int main()
 	int i=0;
 	
 	
-	char v[100] = "youtube.com.br/watch?v=9EHeSjoDTU8";
-	printf("\n------------BLACK LIST -----------\n");
-	i = checkLists("blacklist.txt","www.netfix.com.br");
-	printf("\nSaida BLACK LIST = %d",i);
-	printf("\n------------BLACK LIST -----------\n");
-	i = checkLists("blacklist.txt","www.youtube.com.br");
-	printf("\nSaida BLACK LIST = %d",i);
-	printf("\n------------BLACK LIST -----------\n");
-	i =  checkLists("blacklist.txt",v);
-	printf("\nSaida BLACK LIST = %d",i);
-	printf("\n------------DENY TERMS LIST -----------\n");
-	i = checkLists("denyterms.txt","gatinhos");
-	printf("\nSaida BLACK LIST = %d",i);
-	i = checkLists("denyterms.txt","fofinhos");
-	printf("\nSaida BLACK LIST = %d",i);
-	i = checkLists("denyterms.txt","fofidsadanhos");
-	printf("\nSaida BLACK LIST = %d",i);
-	i = checkLists("denyterms.txt","FORA temer");
-	printf("\nSaida BLACK LIST = %d",i);
-	i = checkLists("denyterms.txt","fora temer");
-	printf("\nSaida BLACK LIST = %d",i);
-	i = filtragem_mensagem("aprender.unb.br");
-	printf("\nSaida FILTRO = %d",i);
-	i = filtragem_mensagem(v);
-	printf("\nSaida FILTRO = %d",i);
-	Length_denyterms();
-	
-		printf("\n====-==== COMECANDO ");
-	char v2[] = "www.pt.org.br/tag/fora-temer";
-	i = denyterms_request(v2);
-	printf("\n====-==== %d",i);
-	char v1[2000] = "https://www.google.com.br/search?ei=XCYKWvC0K4iHwgSs95jQAQ&q=fofInhos&oq=foFinhos&gs_l=psy-ab.3..0l10.5084.6085.0.8457.3.3.0.0.0.0.212.453.0j2j1.3.0....0...1.1.64.psy-ab..0.3.451...0i10k1j0i5i10i30k1j0i67k1.0.G0-j9hW_znQ";
-	i = denyterms_request(v1);
-	printf("\n====-==== %d",i);
+	char v[100] = "http://www.youtube.com.br/watch?v=9EHeSjoDTU8";
+	printf("\n====-==== COMECANDO ");
+	char v2[] = "http://www.pt.org.br/tag/fora-temer";
+	i=filtragem_url(v2);
+	printf("\n saida = %d ",i);
+	char v1[] = "http://www.google.com.br/search?ei=XCYKWvC0K4iHwgSs95jQAQ&q=fofInhos&oq=foFinhos&gs_";
+	i=filtragem_url(v1);
+	printf("\n saida = %d ",i);
+	i=filtragem_url(v);
+	printf("\n saida = %d ",i);	
+	char w[]= "http://www.unb.br";
+	i=filtragem_url(w);
+	printf("\n saida = %d ",i);	
+	char g[] = "http://www.globo.br";
+	i=filtragem_url(g);
+	printf("\n saida = %d ",i);	
+	printf("\n FIM");
 	return i;
 }
 */
@@ -61,8 +44,7 @@ int checkLists(char* nome_arquivo,char * mensagem){
 	
 	char procurado[strlen(mensagem)];
  	// zerando a string
- 	printf("\n%s lista[%d] = %s\n",procurado,i,lista[i]);
-	memset (procurado,'\0',strlen(mensagem));
+ 	memset (procurado,'\0',strlen(mensagem));
 	strcpy (procurado,mensagem);
 	FILE * fp = abrindo_arquivo(nome_arquivo);
 
@@ -82,48 +64,68 @@ int checkLists(char* nome_arquivo,char * mensagem){
 	return 0;
 	}
 //por enquanto retorna um int para indicar se ta ou nao ta proibido
-int filtragem_mensagem(char * site_destino)
+int filtragem_url(char * url)
 {
 	// pre processamento do host ---- adicionando www.
 	char host_www[100];
-	strcpy(host_www,site_destino);
+	strcpy(host_www,url);
 	remove_http(host_www);
 	// primeiro verifica-se se esta na whiteList
-	int aux,aux2;
-	aux = checkLists("whitelist.txt",host_www);
-	aux2= checkLists("blacklist.txt",host_www);
-	if(aux){
-		// aux = 1, site pertence a lista branca
-		//saida = 1 indica que pode se encaminhar a mensagem ao destinatario
-		// chama funcao que encaminha mensagem ....
+			
+	int aux_white,aux_black,aux_deny;
+	aux_white = checkLists("whitelist.txt",host_www);
+	aux_black= checkLists("blacklist.txt",host_www);
+	aux_deny = denyterms_request(host_www);
+	if(aux_white){
+		/* aux = 1, site pertence a lista branca
+		*saida = 1 indica que pode se encaminhar a mensagem ao destinatario
+		* chama funcao que encaminha mensagem ....
+		* neste caso nao se retornaria nada 
+		* deveria chamar a funcao para que enviar e escrever log
+		*/
+		printf("\n\tWHITE LIST OK --- ENCAMINHAR MENSAGEM %s \n",host_www);
+		
 		return 1;
 	} // verificando se esta na lista negra
-	else if(aux2){
-		// aux 2 indica que esta na lista proibida e saida = 0 indica que deve-se descartar a requesicao
+	else if(aux_black){
+	   /* aux 2 indica que esta na lista proibida e saida = 0 indica que deve-se descartar a requesicao
+		* deveria chamar a funcao para que seja reijado e escrever log
+		*/
+		printf("\n\tESTA NA BLACK LIST  --- REJEITAR MENSAGEM %s \n",host_www);
 		return 0;
 	 }// se nao tiver em nenhuma da lista deve-se procurar por termos proibidos
-	else{
-	 	//buscar pelos deny terms
-	 	// tem que analisar o corpo do html
-		// mais pra frente ....  	
-		return 2000;
+	else if (aux_deny){
+	 	// se for 1 entao tem termos proibidos na url 
+		printf("\n\tdeny terms na URL  --- REJEITAR  MENSAGEM %s \n",host_www);
+		return 0;	
+	}else{
+			printf("\n\tNao eh proibido nem esta na white - ENCAMINHAR MENSAGEM %s \n",host_www);
+			return 1;
 	}
 	
+	
 }
-void remove_http(char* site_destino){
+void remove_http(char* url_http){
 	// recebe url com http://
 	int contador = 6,i=0;
 
-	char www[100];
-	if(strnicmp(site_destino,"http://",7)==0)
-	{
-		for(i=0,contador = 6; contador < strlen(site_destino); contador++,i++)
-			www[i] = site_destino[contador];
+	char url_www[100];
+	if(strnicmp(url_http,"http://",7)==0)
+	{	
+		
+		for(i=0,contador = 7; contador < strlen(url_http); contador++,i++)
+			{
+			url_www[i] = url_http[contador];
+			}
+			strcpy(url_http,url_www);
+	}else if(strnicmp(url_http,"https://",8)==0){
+		printf("\n ERRO nao eh  aceito https \n");
+		exit(0);
 	}else{
-		printf("ERRO nao eh http \n");
+		printf("Nao reconhecido o cabecalho \n");
+		exit(0);
 	}
 
-	strcpy(site_destino,www);
 	
 }
 
@@ -149,17 +151,21 @@ int denyterms_request(char * request){
 					aux_s[k] = request[i_aux];		
 				}
 			}		
-			printf("\nDENTR == %d \n",tamanho[j]);
+		
 	       if (checkLists("denyterms.txt",aux_s))
 		 	{
-		 	printf("\nDENTRO IF \t\t DEBUG == %s /// %d\n",aux_s,tamanho[j]);
-		 	return 1;
+		 	//printf("\nDENTRO IF \t\t DEBUG == %s /// %d\n",aux_s,tamanho[j]);
+		 	free(aux_s);
+		 	free(tamanho);
+			 return 1;
 			}
 		
 		}
 		}
-	
+	free(aux_s);
+	free(tamanho);
 	return 0;
+	
 }
 
 int * Length_denyterms(){
@@ -178,9 +184,15 @@ int * Length_denyterms(){
 		i++;j++;
 	} 
 	vetor[0] = i;  
-	//printf("\n OLA %d ----- %d\n",vetor[0],(strlen(lista[i])-1));
+	// para o ultimo termo do denyterms deve-se contar o ultimo elemento buga porcausa do EOF
+	vetor[i]++;
+	/*printf("\n OLA %d ----- %d\n",vetor[0],(strlen(lista[i])-1));
+	 for(j = 0;j <= i; j++){
+			printf("\n\t\tVetor[%d] = %d",j,vetor[j]);
+
+		}
+	*/
 	fclose(fp);
 	return vetor;
 	
 }
-
--- filtragem.h
+++ filtragem.h
@@ -6,13 +6,13 @@
 #include <stdio.h>
 
 #define SIZE_LISTA 30
-#define MAX_STR 100
+#define MAX_STR 500
 
 FILE* abrindo_arquivo(char* nome_arquivo);
 int checkLists(char* nome_arquivo,char * mensagem);
 // talvez seja melhor receber a mensagem toda para que depois possa analizar o deny terms
-int  filtragem_mensagem(char * site_destino);
-void remove_http(char* site_host);
+int  filtragem_url(char * url);
+void remove_http(char* url_http);
 int denyterms_request(char * request);
-int * Length_denyterms();
+int * Length_denyterms(void);
 #endif
