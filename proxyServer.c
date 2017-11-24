9cbb0a2d28cc07a1d6b0268a11eec7ad8958a521
Lucas Santos <lucasnascimentoss@gmail.com>
2017-11-24 11:42:02 -0200

    Struct inicial cabecalho e erros

--- proxyServer.c
+++ proxyServer.c
@@ -9,88 +9,128 @@
 #include<netinet/in.h>
 #include<errno.h>
 
-#define BACKLOG 20     // how many pending connections queue will hold
+/*
+ Erros:
+    -1 = numero da porta
+    -2 = descritor socket negativo
+    -3 = erro associacao
+    -4 = erro escutar
+    -5 = erro aceitacao
+    -6 = erro dados
+*/
+
+// TODO: DEFINIR BACKLOG E TAMBEM SE TERA UMA PORTA PADRAO OU NAO
+
+// numero de conexoes pendentes na fila
+#define BACKLOG 20
+
+/*
+ Struct para armazenar os campos do cabecalho
+*/
+typedef struct header_files{
+    int last-modified;
+    int if-modified-since;
+    int set-cookie;
+    int content-type;
+    int host;
+    int user-agent;
+    int x-content-type-options;
+    int retry-after;
+} Header;
 
 int main(int argc, char* argv[]){
-// TODO: DEFINIR BACKLOG E TAMBEM SE TERA UMA PORTA PADRAO OU NAO
-if(argc < 2)
-{
-	perror("Para executar ./proxy <numero_da_porta> \n Lembrando que eh porta utilizada pelo IP do LoopBack");
-	exit(0);
-}
-struct sockaddr_in endereco,server_end,client_end;
 
-printf("Começando a escutar a conexao\n");
-/*
-FLUXO DAS FUNCOES 
-socket();
-bind();
-listen();
- accept() goes here 
- */ 
-
-//botando os valores na struct da sock addr
-// padrao
-server_end.sin_family = AF_INET;
-// pegando a porta passada como parametro e associando ao server
-server_end.sin_port = htons(atoi(argv[1]));
-// utilizando o loopback como endereco
-server_end.sin_addr.s_addr =  inet_addr("127.0.0.1");
-// o resto da struct nao interessa
-memset(&(server_end.sin_zero), '\0', 8); // zero the rest of the struct
-int num_socket;
-//int socket(int domain, int type, int protocol); 
-num_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-// retorna um valor para descrever a socket
-if(num_socket < 0)
-{
-	perror("Socket nao valido, valor negativo retornado");
-	exit(0);
-}
-// tendo a socket, devemos associar ela a uma porta
-int auxERRO;
-//int bind(int sockfd, struct sockaddr *my_addr, int addrlen); 
-auxERRO = bind(num_socket,(struct sockaddr *)&server_end,sizeof(server_end));
-if(auxERRO < 0)
-	{
-	perror("Èrro ao associar porta, valor negativo retornado");
-	exit(0);
-	}		
-
-auxERRO = listen(num_socket,BACKLOG);
-if( auxERRO < 0)
-	{
-	perror("Èrro ao escutar a porta\n");
-	exit(0);
-	}		
-int sin_size = sizeof(client_end);
-int new_accept_sock;
-char buffer[6000];
-
-// TODO :: CONTINUAR
-// aceitando a socket 	
-new_accept_sock = accept(num_socket,(struct sockaddr *)&client_end,&sin_size);
-//new_accept_sock = connect(num_socket, (struct sockaddr *)&server_end, sizeof(struct sockaddr));
-
-/* TODO : CONTINUAR 
- * RELATIVE : Executando uma vez, recebe o metodo GET ... Mas nao acessa a pagina	
- * NEW_SOCKET_NUM que esta mostrando o buffer
- * DEPOIS O PROGRAMA FECHA 
-
-if( recv(num_socket, buffer, 6000 , 0) < 0)
-    {
-        printf("falhou socket nao aceita \n\n");
-    }else{
-    printf("Resposta recebida da socket nao aceita\n");
-    puts(buffer);	
-	}
-if( recv(new_accept_sock, buffer, 6000 , 0) < 0)
-    {
-        printf("recv failed da socket aceita");
-    }else{
-    printf("Resposta recebida da socket aceita\n");
-    puts(buffer);
+    if(argc < 2){
+        perror("Para executar ./proxy <numero_da_porta> \n Lembrando que eh porta utilizada pelo IP do LoopBack");
+        exit(-1);
     }
+
+    printf("Comecando a escutar a conexao\n");
+
+    struct sockaddr_in endereco, server_end, client_end;
+    
+    /* 
+     Fluxo das funcoes: 
+        socket();
+        bind();
+        listen();
+        accept(); 
+    */ 
+
+    /* Associando os valores a struct sokadr_in server_send */
+    // padrao
+    server_end.sin_family = AF_INET;
+    // pegando a porta passada como parametro e associando ao server
+    server_end.sin_port = htons(atoi(argv[1]));
+    // utilizando o loopback como endereco
+    server_end.sin_addr.s_addr =  inet_addr("127.0.0.1");
+    // zerando o resto da struct
+    memset(&(server_end.sin_zero), '\0', 8); 
+    
+    /* Criacao da socket */
+    // int socket(int domain, int type, int protocol)
+    //  - retorna um valor para descrever a socket
+    int num_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if(num_socket < 0){
+        perror("Socket nao valido, valor negativo retornado\n");
+        exit(-2);
+    }
+
+    /* Associando uma porta a socket */
+    // int bind(int sockfd, struct sockaddr *my_addr, int addrlen);
+    //  - retorna negativo se der erro 
+    int auxERRO = bind(num_socket,(struct sockaddr *)&server_end,sizeof(server_end));
+    if(auxERRO < 0){
+        perror("Erro ao associar porta, valor negativo retornado\n");
+        exit(-3);
+    }		
+
+    /* Preparaco para conexoes */
+    // int bind(int sockfd, int backlog);
+    //  - retorna negativo se der erro 
+    auxERRO = listen(num_socket,BACKLOG);
+    if(auxERRO < 0){
+        perror("Erro ao escutar a porta, valor negativo retornado\n");
+        exit(-4);
+    }		
+
+    int sin_size = sizeof(client_end);
+    int new_accept_sock;
+    char buffer[6000];
+
+    // TODO :: CONTINUAR
+
+    /* Aceitando a socket */	
+    // accept(num_socket, (struct sockaddr *)&server_end, sizeof(struct sockaddr));
+    //  - retorna negativo se der erro 
+    new_accept_sock = accept(num_socket,(struct sockaddr *)&client_end,&sin_size);
+    if(auxERRO < 0){
+        perror("Erro ao aceitar a socket, valor negativo retornado\n");
+        exit(-5);
+    }	
+
+    // TODO :: CONTINUAR
+
+    /*  
+     RELATIVE : Executando uma vez, recebe o metodo GET ... Mas nao acessa a pagina	
+     NEW_SOCKET_NUM que esta mostrando o buffer
+     DEPOIS O PROGRAMA FECHA 
+
+    if(recv(num_socket, buffer, 6000 , 0) < 0){
+        printf("falhou socket nao aceita \n\n");
+    }
+    else{
+        printf("Resposta recebida da socket nao aceita\n");
+        puts(buffer);	
+    }
+    if(recv(new_accept_sock, buffer, 6000 , 0) < 0){
+        printf("recv failed da socket aceita");
+    }
+    else{
+        printf("Resposta recebida da socket aceita\n");
+        puts(buffer);
+    }
+        
     // tentando verificar um padrao 
     char GET[10],URL[120],HTTP[15],HOST[10];
     sscanf(buffer,"%s %s %s %s",GET,URL,HTTP,HOST);
@@ -98,40 +138,45 @@ if( recv(new_accept_sock, buffer, 6000 , 0) < 0)
     printf("URL == %s\n", URL);
     printf("HTTP == %s\n", HTTP);
     printf("HOST == %s\n",HOST);
+    
     */ 
-// USANDO pid para passar o cliente para o processo filho
-struct hostent * host;
-pid_t pid = fork();
+    
+    // USANDO pid para passar o cliente para o processo filho
+    struct hostent * host;
+    pid_t pid = fork();
 
-if(!pid){
+    if(!pid){
+        if(recv(new_accept_sock, buffer, 6000 , 0) < 0){
+            printf("Erro ao receber dados da socket aceita, valor negativo retornado\n");
+            exit(-6);
+        }
+        else{
+            printf("Resposta recebida da socket aceita\n");
+            puts(buffer);
+        }
+        
+        // tentando verificar um padrao 
 
-if( recv(new_accept_sock, buffer, 6000 , 0) < 0)
-    {
-        printf("recv failed da socket aceita");
-    }else{
-    printf("Resposta recebida da socket aceita\n");
-    puts(buffer);
-    }
-    // tentando verificar um padrao 
-    // TODO: depois modularizar para alterar e pa 
-    char GET[10],URL[120],HTTP[15],HOST_fix[10],HOST[100];
-    sscanf(buffer,"%s %s %s %s %s",GET,URL,HTTP,HOST_fix,HOST);
-    printf("GET == %s\n", GET);
-    printf("URL == %s\n", URL);
-    printf("HTTP == %s\n", HTTP);
-    if((strcmp(GET,"GET")!=0)|| ((strcmp(HTTP,"HTTP/1.0"))!= 0 && (strcmp(HTTP,"HTTP/1.1"))))
-    {	// SE FOR DIFERENTE DE GET = 1
-    	// SE NAO FOR HTTP1.0 e NEM HTTP1.1 = (PORTA E)1 
-       	printf("Cabecalho nao corresponde ao padrao ... \n");
-    	// TODO: fazer log de erro .... ou enviar mensagem de erro
-   		close(new_accept_sock);
-    } 
-    // esta eh a primeira linha sempre sera comecara com isso
-    printf("HOST == %s\n",HOST_fix);
-    printf("valor do HOST = %s\n",HOST);
-	
-}    
+        // TODO: depois modularizar para alterar e pa 
+        
+        char GET[10],URL[120],HTTP[15],HOST_fix[10],HOST[100];
+        sscanf(buffer,"%s %s %s %s %s",GET,URL,HTTP,HOST_fix,HOST);
+        printf("GET == %s\n", GET);
+        printf("URL == %s\n", URL);
+        printf("HTTP == %s\n", HTTP);
+        
+        if((strcmp(GET,"GET")!=0)|| ((strcmp(HTTP,"HTTP/1.0"))!= 0 && (strcmp(HTTP,"HTTP/1.1")))){
+            // SE FOR DIFERENTE DE GET = 1
+            // SE NAO FOR HTTP1.0 e NEM HTTP1.1 = (PORTA E)1 
+            printf("Cabecalho nao corresponde ao padrao ... \n");
+            // TODO: fazer log de erro .... ou enviar mensagem de erro
+            close(new_accept_sock);
+        } 
+        // esta eh a primeira linha sempre sera comecara com isso
+        printf("HOST == %s\n",HOST_fix);
+        printf("valor do HOST = %s\n",HOST);
+        
+    }    
 
     return 0;
-
 }
