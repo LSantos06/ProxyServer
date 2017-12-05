c65ecacbf0163fff2f629d878502bc4c0a4dc9c9
Lucas Santos <lucasnascimentoss@gmail.com>
2017-12-05 00:35:46 -0200

    :white_check_mark: inspecao

--- inspecao.h
+++ inspecao.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "estruturas.h"
 
 /* Janela inpecao */
 // exibe a janela de inspecao
---
+++ test/teste_inspecao.c
@@ -0,0 +1,8 @@
+#include "../include/inspecao.h"
+
+int main(int argc, char *argv[]){
+
+
+
+    return 0;
+}
